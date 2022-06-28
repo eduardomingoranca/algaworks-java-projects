61a92ea0dcce7034664596d5655a277eed5b821b
eduardomingoranca <eduardomingoranca@gmail.com>
2022-06-28 19:55:31 -0300

    add scape sequence

---
+++ 2.4_sequencias_de_escape/codigos/OiMundo.java
@@ -0,0 +1,10 @@
+public class OiMundo {
+	/*
+	 * Esta linha sera ignorada pelo compilador
+	 * System.out.println("essa instrucao sera ignorada");
+	 * e essa tambem 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Oi \nMundo");
+	}
+}
