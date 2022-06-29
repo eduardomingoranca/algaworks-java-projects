f7526c06aadb54bc2f0cfb757e4df3638a80ee4d
eduardomingoranca <eduardomingoranca@gmail.com>
2022-06-29 19:36:18 -0300

    add variable name

---
+++ 2.8_nomeando_variaveis/codigos/Variavel.java
@@ -0,0 +1,8 @@
+public class Variavel {
+    
+    public static void main(String[] args) {
+        int quantidadeAlunos;
+        int quantidadeAlunosNota10;
+        int numeroDeAlunosAprovados;
+    }
+}
