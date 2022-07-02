f7281e5e33dd9d2391499b45b595b8b3299eb1a1
eduardomingoranca <eduardomingoranca@gmail.com>
2022-07-02 14:46:03 -0300

    add creating, naming and calling methods

---
+++ 3.10_criando_nomeando_e_chamando_metodos/codigos/Carro.java
@@ -0,0 +1,14 @@
+public class Carro {
+
+    String fabricante = "Sem fabricante";
+    String modelo;
+    String cor;
+    int anoDeFabricante = 2011; // 0
+    boolean biCombustivel = true; // false
+    Proprietario dono = new Proprietario(); // null
+
+    void ligar() {
+        System.out.println("Ligando o carro: " + modelo);
+    }
+
+}
