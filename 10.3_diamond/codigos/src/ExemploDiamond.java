b98ed83e2e4a82d17c69bf140c9746d57fa53194
eduardomingoranca <eduardomingoranca@gmail.com>
2022-08-06 11:46:54 -0300

    add diamond

---
+++ 10.3_diamond/codigos/src/ExemploDiamond.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ExemploDiamond {
+
+    public static void main(String[] args) {
+        List<String> nomesClientes = new ArrayList<>();
+        nomesClientes.add("John");
+        nomesClientes.add("Mary");
+
+        Map<String, Set<String>> carrosPorMontadora = new HashMap<>();
+        carrosPorMontadora.put("Chevrolet", new HashSet<String>());
+        carrosPorMontadora.get("Chevrolet").add("Cruze");
+
+        Set<String> carrosDaVolks;
+        carrosPorMontadora.put("Volks", carrosDaVolks = new HashSet<>());
+        carrosPorMontadora.get("Volks").add("Gol");
+        
+    }
+}
